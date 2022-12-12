aa77b5d733288b57fc858f80d07a21660b1bd456
Nihar <sahunihar0602@gmail.com>
2022-12-12 12:30:26 -0500

    Made changes in staging port

--- config/Constants.js
+++ config/Constants.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    PORT: process.env.PORT || 5000,
+    PORT: process.env.PORT || 4000,
     API_VERSION: "/api/v1",
     EMAIL_REGEX: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/,
     SALT_ROUND: 10
